cb9a3e8242aacaabbdc5b274221aa45ac9fb6790
MatSim91 <mateus.r.simoes@gmail.com>
2021-03-12 19:59:05 +0000

    Customize modal pop-up and finished JavaScript code to add and remove show class

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ const open = document.getElementById('open')
 const modal_container_ins = document.getElementById('modal_container_ins')
 const close = document.getElementById('close')
 
+open.addEventListener('click', () => {
+    modal_container_ins.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container_ins.classList.remove('show');
+});
