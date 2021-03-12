26c531f242aed48d7aa7c4b2c9a4a266ebb8a0e0
MatSim91 <mateus.r.simoes@gmail.com>
2021-03-12 19:35:14 +0000

    Created instructions modal and started JavaScript code to show and hide pop-up

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/*---------------------------- Index Page Modal Popup ----------------------------*/
+
+const open = document.getElementById('open')
+const modal_container_ins = document.getElementById('modal_container_ins')
+const close = document.getElementById('close')
+
