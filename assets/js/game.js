5609d8f6b884be900f58226979847f1e8dcf0d4b
MatSim91 <mateus.r.simoes@gmail.com>
2021-03-21 14:17:45 +0000

    Add function runGame

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,6 +5,10 @@
 let card = document.getElementsByClassName("card");
 let cards = [...card];
 
+const deck = document.getElementById("card-deck")
+
+var openedCards = [];
+
 /*--- Toggle classes to open and show the cards ---*/
 
 var displayCard = function (){
@@ -12,6 +16,41 @@ var displayCard = function (){
     this.classList.toggle("show");
     this.classList.toggle("disabled");
 };
+
+/*--- Function to Shuffle the cards ---*/
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
+/*--- Function to run the game ---*/ 
+
+document.body.onload = runGame();
+
+function runGame(){
+    
+    openedCards = [];
+
+    
+    cards = shuffle(cards);
+    
+    for (var i = 0; i < cards.length; i++){
+        deck.innerHTML = "";
+        [].forEach.call(cards, function(item) {
+            deck.appendChild(item);
+        });
+        cards[i].classList.remove("show", "open", "match", "diasbled");
+    }
 
 /*--- For loop adding Event Listener ---*/
 
