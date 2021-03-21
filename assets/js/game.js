38649db2900ea34357cfa0cfd348fff3efbf1fb0
MatSim91 <mateus.r.simoes@gmail.com>
2021-03-21 12:42:04 +0000

    Start memory game JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-/*---------------------------- Game Page ----------------------------*/
+/*--- Based on Sandra Israel Tutorial ---*/
+
+/*--- Declared Variables ---*/
+
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+
+/*--- Toggle classes to open and show the cards ---*/
+
+var displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+};
+
+/*--- For loop adding Event Listener ---*/
+
+for (var i = 0; i < cards.length; i++){
+   cards[i].addEventListener("click", displayCard);
+};
